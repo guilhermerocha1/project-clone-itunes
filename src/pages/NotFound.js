e62d0a9769a36c719c238d2bda07bcecfbcafe57
guilhermerocha1 <guilhermerochasilva318@outlook.com>
2022-01-25 17:59:48 -0300

    Requisito 1

---
+++ src/pages/NotFound.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Notfound extends React.Component {
+  render() {
+    return (
+      <div data-testid="page-not-found">
+        <h1>Notfound</h1>
+      </div>
+    );
+  }
+}
+
+export default Notfound;
