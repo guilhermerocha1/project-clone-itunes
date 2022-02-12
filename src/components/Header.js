258deb58a2ad7cef18b3603768907f18377571b1
guilhermerocha1 <guilhermerochasilva318@outlook.com>
2022-02-12 14:39:27 -0300

    Requisito 3 e 4

---
+++ src/components/Header.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { getUser } from '../services/userAPI';
+import Loading from './Loading';
+
+class Header extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      userName: '',
+    };
+  }
+
+  componentDidMount() {
+    this.userName();
+  }
+
+  userName = async () => {
+    const user = await getUser();
+    this.setState({ userName: user.name });
+  }
+
+  render() {
+    const { userName } = this.state;
+    return (
+      !userName ? <Loading /> : (
+        <header data-testid="header-component">
+          <h2 data-testid="header-user-name">{userName}</h2>
+          <nav>
+            <Link to="/search" data-testid="link-to-search" />
+            <Link to="/favorites" data-testid="link-to-favorites" />
+            <Link to="/profile" data-testid="link-to-profile" />
+          </nav>
+        </header>
+      )
+    );
+  }
+}
+
+export default Header;
